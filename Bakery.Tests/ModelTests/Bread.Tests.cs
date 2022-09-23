98720bc83e2197c9c72b5e6ed2c7b1770bdb024d
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-23 09:56:38 -0700

    WIP first test

--- Bakery.Tests/ModelTests/Bread.Tests.cs
+++ Bakery.Tests/ModelTests/Bread.Tests.cs
@@ -1,18 +1,40 @@
 // Bread Tests
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Scorer.Models;
+using Bread.Models;
 
-// namespace Scorer.Models.Tests
-// {
-//   [TestClass]
-//   public class GameTest
-//   {
-//     [TestMethod]
-//     public void tallyScore_outputWordScore_True()
-//     {
-//       Game gameVariable = new Game();
-//       Assert.AreEqual(21, gameVariable.tallyScore("butterscotch"));
-//     }
-//   }
-// }
+namespace Bread.Models.Tests
+{
+  [TestClass]
+  public class BreadCostTest
+  {
+    [TestMethod]
+    public void CalcCost_outputCost_Number()
+    {
+      Order orderVariable = new Order();
+      Assert.AreEqual("5", orderVariable.calcCost("bread"));
+    }
+  }
+}
+
+
+
+//b - calculates cost of bread based on given number
+// - calculates cost of pastry based on given number
+//returns - total cost of the order 
+
+
+//BREAD COST = $5 each, 
+//if bread# = 1,
+// charge $5
+// else if bread# = 2
+// charge $10
+// else if 
+// bread# = 3
+// charge $10
+// else
+// "order is limited to 3 per person"
+
+
+// Ui - User input - number of loafs of bread and loafs of pastry they would like
+
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,51 +4,51 @@ using System;
 using System.Collections.Generic;
 
 
-// namespace Scorer.Models
-// {
-//   public class Game
-//   {
-//     public int tallyScore(string word)
-//     {
-//       int score = 0;
-//       string unusedVar = word;
-//       // Version 1: use foreach-loop.
-//       foreach (char c in word)
-//       {
-//         if (c.Equals('a') || c.Equals('e') || c.Equals('i') || c.Equals('o') || c.Equals('u') || c.Equals('l') || c.Equals('n') || c.Equals('r') || c.Equals('s') || c.Equals('t'))
-//         {
-//           score += 1;
-//         }
-//         else if (c.Equals('d') || c.Equals('g'))
-//         {
-//           score += 2;
-//         }
-//         else if (c.Equals('b') || c.Equals('c') || c.Equals('m') || c.Equals('p'))
-//         {
-//           score += 3;
-//         }
-//         else if (c.Equals('f') || c.Equals('h') || c.Equals('v') || c.Equals('w') || c.Equals('y'))
-//         {
-//           score += 4;
-//         }
-//         else if (c.Equals('k'))
-//         {
-//           score += 5;
-//         }
-//         else if (c.Equals('j') || c.Equals('x'))
-//         {
-//           score += 8;
-//         }
-//         else if (c.Equals('q') || c.Equals('z'))
-//         {
-//           score += 10;
-//         }
-//         else
-//         {
-//           Console.WriteLine(c + " is not a character in the game, ignoring it");
-//         }
-//       }
-//       return score;
-//     }
-//   }
-// }
+namespace Bread.Models
+{
+  public class BreadCost
+  {
+    public int calcCost(string word)
+    {
+      int score = 0;
+      string unusedVar = word;
+      // Version 1: use foreach-loop.
+      foreach (char c in word)
+      {
+        if (c.Equals('a') || c.Equals('e') || c.Equals('i') || c.Equals('o') || c.Equals('u') || c.Equals('l') || c.Equals('n') || c.Equals('r') || c.Equals('s') || c.Equals('t'))
+        {
+          score += 1;
+        }
+        else if (c.Equals('d') || c.Equals('g'))
+        {
+          score += 2;
+        }
+        else if (c.Equals('b') || c.Equals('c') || c.Equals('m') || c.Equals('p'))
+        {
+          score += 3;
+        }
+        else if (c.Equals('f') || c.Equals('h') || c.Equals('v') || c.Equals('w') || c.Equals('y'))
+        {
+          score += 4;
+        }
+        else if (c.Equals('k'))
+        {
+          score += 5;
+        }
+        else if (c.Equals('j') || c.Equals('x'))
+        {
+          score += 8;
+        }
+        else if (c.Equals('q') || c.Equals('z'))
+        {
+          score += 10;
+        }
+        else
+        {
+          Console.WriteLine(c + " is not a character in the game, ignoring it");
+        }
+      }
+      return score;
+    }
+  }
+}
